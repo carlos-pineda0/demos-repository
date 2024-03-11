06baf3ac335a4607860ce06eadcd39c614264a89
carlos-pineda0 <160975236+carlos-pineda0@users.noreply.github.com>
2024-03-11 16:19:46 -0500

    ..

--- SB and OAuth2/simple/src/main/java/com/example/simple/SecurityConfig.java
+++ SB and OAuth2/simple/src/main/java/com/example/simple/SecurityConfig.java
@@ -8,11 +8,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
 
     @Bean
     public DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,10 +23,16 @@ public class SecurityConfig {
                                 .requestMatchers("/", "/error", "/webjars/**").permitAll()
                                 .anyRequest().authenticated()
                 )
-//                .exceptionHandling(e -> e
-//                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-//
-//                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
                 .oauth2Login(Customizer.withDefaults());
 
         return http.build();
